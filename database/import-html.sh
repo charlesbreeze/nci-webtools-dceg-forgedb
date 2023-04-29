fb70e8510e1f3899112c668dcb767eda31cfa2f3
Brian Park <park-brian@users.noreply.github.com>
2023-04-29 19:33:43 -0400

    update import-html script

--- database/import-html.sh
+++ database/import-html.sh
@@ -7,14 +7,16 @@ OUTPUT_FOLDER=$TEMP_FOLDER/output
 S3_SOURCE_PATH=$1
 S3_DESTINATION_PATH=$2
 
-# Create the output and logs folders
+# Create the output folder
 mkdir -p $OUTPUT_FOLDER
 
 # Download the source file from S3
-aws s3 cp $S3_SOURCE_PATH $INPUT_FILEPATH
+aws --quiet s3 cp $S3_SOURCE_PATH $INPUT_FILEPATH
 
 # Extract the source file using pigz/tar 
 tar -I pigz -xf $INPUT_FILEPATH --strip-components=3 -k -C $OUTPUT_FOLDER
 
 # Upload the extracted source file to S3
 aws --quiet s3 sync $OUTPUT_FOLDER $S3_DESTINATION_PATH
+
+echo "Done"

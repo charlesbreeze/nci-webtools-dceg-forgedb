1bfd977ffe28d58b3580a0e44b6d59a394081d71
Brian Park <park-brian@users.noreply.github.com>
2023-04-29 23:47:46 -0400

    use serial import due to rate limits

--- database/import-html.sh
+++ database/import-html.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 set -ex
 
-TEMP_FOLDER=$(mktemp -d)
-INPUT_FILEPATH=$TEMP_FOLDER/input.tar.gz
-OUTPUT_FOLDER=$TEMP_FOLDER/output
-S3_SOURCE_PATH=$1
-S3_DESTINATION_PATH=$2
+# Get the list of files to import
+files=$(aws s3 ls $S3_SOURCE_PATH/ | awk '{print $4}' | grep --color=never "htmls.tar.gz")
 
-# Set max concurrent requests (higher = faster for larger numbers of small files)
-aws configure set default.s3.max_concurrent_requests 20
+# For each file, run the import
+# Note that this is not parallelized due to PutObject rate limits on the same key prefix
+for file in $files
+do
+  TEMP_FOLDER=$(mktemp -d)
+  INPUT_FILEPATH=$TEMP_FOLDER/input.tar.gz
+  OUTPUT_FOLDER=$TEMP_FOLDER/output
 
-# Create the output folder
-mkdir -p $OUTPUT_FOLDER
+  # Set max concurrent requests (higher = faster for larger numbers of small files)
+  aws configure set default.s3.max_concurrent_requests 100
 
-# Download the source file from S3
-aws s3 cp --only-show-errors $S3_SOURCE_PATH $INPUT_FILEPATH
+  # Create the output folder
+  mkdir -p $OUTPUT_FOLDER
 
-# Extract the source file using pigz/tar 
-tar -I pigz -xf $INPUT_FILEPATH --strip-components=3 -k -C $OUTPUT_FOLDER
+  # Download the source file from S3
+  aws s3 cp --only-show-errors $S3_SOURCE_PATH/$file $INPUT_FILEPATH
 
-# Upload the extracted source file to S3
-aws s3 cp --recursive --exclude "*" --include "*.html" --only-show-errors $OUTPUT_FOLDER $S3_DESTINATION_PATH
+  # Extract the source file using pigz/tar 
+  tar -I pigz -xf $INPUT_FILEPATH --strip-components=3 -k -C $OUTPUT_FOLDER
 
-echo "Done"
+  # Upload the extracted source file to S3
+  aws s3 cp --recursive --exclude "*" --include "*.html" --only-show-errors $OUTPUT_FOLDER $S3_DESTINATION_PATH
+
+  # Remove the temporary directory
+  rm -rf $TEMP_FOLDER
+
+  echo "Extracted $file to $S3_DESTINATION_PATH"
+done

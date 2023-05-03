7ced93a8bf47be9358fcd5b29d88c99e1beb78af
Brian Park <park-brian@users.noreply.github.com>
2023-05-02 22:13:45 -0400

    add success flag and configurable source files

--- database/import-html.sh
+++ database/import-html.sh
@@ -6,22 +6,32 @@ aws configure set default.s3.max_concurrent_requests 100
 aws configure set default.retry_mode standard
 aws configure set default.max_attempts 20
 
-# Get the list of files to import
-files=$(aws s3 ls $S3_SOURCE_PATH/ | awk '{print $4}' | grep --color=never "htmls.tar.gz")
+# Get the list of files to import if not already provided
+if [ -z "$S3_SOURCE_FILES" ]
+then
+  S3_SOURCE_FILES=$(aws s3 ls $S3_SOURCE_PATH/ | awk '{print $4}' | grep --color=never "htmls.tar.gz$")
+fi
 
 # For each file, run the import
 # Note that this is not parallelized due to PutObject rate limits on the same key prefix
-for file in $files
+for S3_FILE in $S3_SOURCE_FILES
 do
   TEMP_FOLDER=$(mktemp -d)
   INPUT_FILEPATH=$TEMP_FOLDER/input.tar.gz
   OUTPUT_FOLDER=$TEMP_FOLDER/output
 
+  # Skip if the success file exists
+  if aws s3 ls $S3_DESTINATION_PATH/$S3_FILE.success
+  then
+    echo "Skipping $S3_FILE"
+    continue
+  fi
+
   # Create the output folder
   mkdir -p $OUTPUT_FOLDER
 
   # Download the source file from S3
-  aws s3 cp --only-show-errors $S3_SOURCE_PATH/$file $INPUT_FILEPATH
+  aws s3 cp --only-show-errors $S3_SOURCE_PATH/$S3_FILE $INPUT_FILEPATH
 
   # Extract the source file using pigz/tar 
   tar -I pigz -xf $INPUT_FILEPATH --strip-components=3 -k -C $OUTPUT_FOLDER
@@ -29,8 +39,12 @@ do
   # Upload the extracted source file to S3
   aws s3 cp --recursive --exclude "*" --include "*.html" --only-show-errors $OUTPUT_FOLDER $S3_DESTINATION_PATH
 
+  # Upload success file
+  touch $TEMP_FOLDER/$S3_FILE.success
+  aws s3 cp --only-show-errors $TEMP_FOLDER/$S3_FILE.success $S3_DESTINATION_PATH/$S3_FILE.success
+  
   # Remove the temporary directory
   rm -rf $TEMP_FOLDER
 
-  echo "Extracted $file to $S3_DESTINATION_PATH"
+  echo "Extracted $S3_FILE to $S3_DESTINATION_PATH"
 done

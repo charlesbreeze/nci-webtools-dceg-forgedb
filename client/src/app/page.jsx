e4c1860420b1ffa1a1027f727e2c88a518be77e5
Ben Chen <benchen4998@gmail.com>
2023-07-25 10:12:40 -0400

    move client to nextjs

---
+++ client/src/app/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+import Link from 'next/link';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-primary-dark">
+        <div className="cover-image">
+          <Container>
+            <Row>
+              <Col md={12}>
+                <div className="d-flex h-100 text-align-center align-items-center py-5">
+                  <div>
+                    <h1 className="font-title text-light mb-3">Visualization SNP</h1>
+                    <hr className="border-white" />
+                    <p className="lead text-light">Explore Visualization SNP Databases.</p>
+                    <Link href="/explore" className="btn btn-lg btn-outline-light text-decoration-none">
+                      Explore Database
+                    </Link>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+
+      <div className="bg-light py-5">
+        <Container>
+          <Row>
+            <Col>
+              <p>
+                A web-based tool designed to enable the exploration of DNase I tag (chromatin accessibility) signal surrounding GWAS array SNPs and the calculation of significance of overlap with transcription factor binding sites from common TF databases.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/navbar/navbar.jsx
@@ -0,0 +1,42 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import clsx from "clsx";
+import NavbarSearch from "./navbar-search";
+
+function pathsMatch(path1, path2) {
+  // remove trailing slash
+  path1 = path1.replace(/\/$/, "");
+  path2 = path2.replace(/\/$/, "");
+  return path1 === path2;
+}
+
+export default function AppNavbar({ routes = [] }) {
+  const pathName = usePathname();
+  return (
+    <Navbar bg="dark" variant="dark" className="text-uppercase font-title" expand="md">
+      <Container>
+        <Navbar.Brand as={Link} href="/" className="d-flex d-md-none text-light">
+          HPV Visuals{" "}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" className="px-0 py-3 text-uppercase">
+          <i className="bi bi-list me-1"></i>
+          Menu
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbar-nav" className="align-items-stretch">
+          <Nav className="me-auto">
+            {routes.map((route) => (
+              <Link className={clsx("nav-link", pathsMatch(pathName, route.path) && "active")} key={route.path} href={route.path}>
+                {route.title}
+              </Link>
+            ))}
+          </Nav>
+          <NavbarSearch />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}

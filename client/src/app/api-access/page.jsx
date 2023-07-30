8ab946a314d907b8cae526ee55f3d1036e13221b
Brian Park <park-brian@users.noreply.github.com>
2023-07-29 23:16:20 -0400

    implement openapi component

--- client/src/app/api-access/page.jsx
+++ client/src/app/api-access/page.jsx
@@ -2,6 +2,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import SwaggerUI from "swagger-ui-react"
 
 export default function ApiAccess() {
   return (
@@ -11,6 +12,7 @@ export default function ApiAccess() {
           <article className="shadow p-4 rounded">
             <h1 className="text-primary h3 mb-4">API Access</h1>
             <hr/>
+            <SwaggerUI url={`${process.env.NEXT_PUBLIC_BASE_PATH}/api/openapi.json`} />
           </article>
         </Col>
       </Row>

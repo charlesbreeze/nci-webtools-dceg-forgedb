0f4ab17991f1d17e9610f7e09a43648591b9e54d
Brian Park <park-brian@users.noreply.github.com>
2023-09-26 18:41:07 -0400

    update logging

--- database/import.js
+++ database/import.js
@@ -1,7 +1,7 @@
 import { createGunzip } from "zlib";
 import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
-import { parse } from 'csv-parse';
-import dotenv from 'dotenv';
+import { parse } from "csv-parse";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -9,36 +9,54 @@ const bucket = process.env.S3_BUCKET_NAME;
 const key = process.env.S3_OBJECT_KEY;
 
 if (!bucket || !key) {
-  throw new Error('Missing required environment variables');
+  throw new Error("Missing required environment variables");
 }
 
 console.log(`Input object key: s3://${bucket}/${key}`);
-const keyParts = key.split('/');
+const keyParts = key.split("/");
 const [datasetName, version] = keyParts.slice(-3, -1);
-const outputKeyPrefix = keyParts.slice(0, -1).join('/') + '/';
-const indexKey = `${keyParts.slice(0, -3).join('/')}/datasets.json`
+const outputKeyPrefix = keyParts.slice(0, -1).join("/") + "/";
+const indexKey = `${keyParts.slice(0, -3).join("/")}/datasets.json`;
+const logKey = `${outputKeyPrefix}${keyParts.at(-1)}.log`;
 
-if (datasetName === 'api' || version === 'api') {
-  throw new Error('Invalid dataset name or version: ');
+if (datasetName === "api" || version === "api") {
+  throw new Error("Invalid dataset name or version: ");
 }
 
 console.log(`Output object key prefix: s3://${bucket}/${outputKeyPrefix}`);
 console.log(`Index key: s3://${bucket}/${indexKey}`);
+console.log(`Log key: s3://${bucket}/${logKey}`);
 
 async function main() {
-  const s3Client = new S3Client({ 
+  const logs = [];
+  const log = async (message) => {
+    const logMessage = `[${new Date().toISOString()}] ${message}`
+    console.log(logMessage);
+    logs.push(logMessage);
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: bucket,
+        Key: logKey,
+        Body: logs.join("\n"),
+      })
+    );
+  }
+
+  const s3Client = new S3Client({
     maxAttempts: 40,
-    retryMode: 'adaptive',
+    retryMode: "adaptive",
   });
 
-  const response = await s3Client.send(new GetObjectCommand({
-    Bucket: bucket,
-    Key: key,
-  }));
+  const response = await s3Client.send(
+    new GetObjectCommand({
+      Bucket: bucket,
+      Key: key,
+    })
+  );
 
   const gunzip = createGunzip();
   const parser = parse({
-    delimiter: ',',
+    delimiter: ",",
     columns: true,
     bom: true,
   });
@@ -49,14 +67,14 @@ async function main() {
   let previousRsid = null;
   let previousItems = [];
   let buffer = [];
-  const bufferSize = 100;
+  const bufferSize = 300;
   const logInterval = 100000;
 
   const flushBuffer = async () => {
     await Promise.all(buffer.map((args, i) => s3Client.send(new PutObjectCommand(args))));
     objectCount += buffer.length;
     buffer = [];
-  }
+  };
 
   for await (const item of response.Body.pipe(gunzip).pipe(parser)) {
     // initialize previousRsid if it's null
@@ -89,7 +107,7 @@ async function main() {
       let elapsed = (end - start) / 1000;
       let rate = i / elapsed;
       let objectRate = objectCount / elapsed;
-      console.log(`Processed ${i} rows (${objectCount} objects) in ${elapsed.toFixed(2)}s (${rate.toFixed(2)} rows/s) (${objectRate.toFixed(2)} objects/s)`);
+      await log(`Processed ${i} rows (${objectCount} objects) in ${elapsed.toFixed(2)}s (${rate.toFixed(2)} rows/s) (${objectRate.toFixed(2)} objects/s)`);
     }
 
     i++;
@@ -99,7 +117,7 @@ async function main() {
     const rsid = previousItems[0].rsid;
     buffer.push({
       Bucket: bucket,
-      Key:  `${outputKeyPrefix}${rsid}.json`,
+      Key: `${outputKeyPrefix}${rsid}.json`,
       Body: JSON.stringify({
         rsid,
         data: previousItems,
@@ -113,13 +131,15 @@ async function main() {
   let elapsed = (end - start) / 1000;
   let rate = i / elapsed;
   let objectRate = objectCount / elapsed;
-  console.log(`Processed ${i} rows (${objectCount} objects) in ${elapsed.toFixed(2)}s (${rate.toFixed(2)} rows/s) (${objectRate.toFixed(2)} objects/s)`);
+  await log(`Processed ${i} rows (${objectCount} objects) in ${elapsed.toFixed(2)}s (${rate.toFixed(2)} rows/s) (${objectRate.toFixed(2)} objects/s)`);
 
   // update the index file
-  const indexResponse = await s3Client.send(new GetObjectCommand({
-    Bucket: bucket,
-    Key: indexKey,
-  }));
+  const indexResponse = await s3Client.send(
+    new GetObjectCommand({
+      Bucket: bucket,
+      Key: indexKey,
+    })
+  );
 
   let indexResponseContents = "";
   for await (const chunk of indexResponse.Body) {
@@ -127,7 +147,7 @@ async function main() {
   }
 
   const datasets = JSON.parse(indexResponseContents);
-  let dataset = datasets.find(d => d.name === datasetName);
+  let dataset = datasets.find((d) => d.name === datasetName);
   if (!dataset) {
     dataset = {
       name: dataset,
@@ -136,14 +156,16 @@ async function main() {
     datasets.push(dataset);
   }
 
-  if (!dataset.versions.find(v => v === version)) {
-    console.log(`Updating s3://${bucket}/${indexKey}`);
+  if (!dataset.versions.find((v) => v === version)) {
+    await log(`Updating s3://${bucket}/${indexKey}`);
     dataset.versions.unshift(version);
-    await s3Client.send(new PutObjectCommand({
-      Bucket: bucket,
-      Key: indexKey,
-      Body: JSON.stringify(datasets, null, 2),
-    }));
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: bucket,
+        Key: indexKey,
+        Body: JSON.stringify(datasets, null, 2),
+      })
+    );
   }
 }
 
